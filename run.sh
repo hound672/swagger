518dbeb380ecf7b4f7b84cbef7e9d0b42a8acb25
Vasilii Bliznetsov <hound672@gmail.com>
2020-03-20 10:43:12 +0300

    Get external port from args

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-docker run -p 8080:8080 \
+#!/bin/bash
+
+docker run -p $1:8080 \
 -e URLS_PRIMARY_NAME=Fastapi \
 -e URLS="[{ url: '/specs/swagger-fastapi.json', name: 'Fastapi'}, { url: '/specs/swagger-test.json', name: 'Test'}]" \
 -v "$PWD":/usr/share/nginx/html/specs/ swaggerapi/swagger-ui
